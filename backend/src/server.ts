062b29bc2a563041c7560e0239f8bfeebc72796b
kaushik gurung <grg23kaushik@gmail.com>
2025-04-09 20:38:23 +0545

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,5 +12,7 @@ app.use(cors({ credentials: true, origin: 'https://task-manager-ruddy-rho.vercel
 
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(5000, () => console.log('Server running on port 5000'));
