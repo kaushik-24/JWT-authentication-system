d7225494af6e0e4c75b05d7797a1ba5e3e187a7b
kaushik gurung <grg23kaushik@gmail.com>
2025-04-09 18:04:36 +0545

    Update auth.ts

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -30,10 +30,11 @@ router.post('/login', async (req: Request, res: Response) => {
     if (!isValid) return res.status(401).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-    res.cookie('token', token, { httpOnly: true,
+    res.cookie('token', token, { 
+      httpOnly: true,
       secure: true,//requires htpps
-      sameSite: 'none',
-      Partitioned: true,//enables chips for cross-site isolation
+      sameSite: 'none', //allows cross-site requests
+      partitioned: true,//enables chips for cross-site isolation
       maxAge: 60 * 60 * 100,
     });
     res.json({ message: 'Logged in successfully' });
