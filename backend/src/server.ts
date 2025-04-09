80013db8f711b4526b4f394730dfacf7f279fee1
kaushik gurung <grg23kaushik@gmail.com>
2025-04-09 21:11:50 +0545

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,7 @@ app.use(cors({ credentials: true, origin: 'https://task-manager-ruddy-rho.vercel
 
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
